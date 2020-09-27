674b10ab638e0d135783054769544f917822efc5
Sharad Chauhan <sharad@getinfino.com>
2020-09-27 16:44:11 +0530

    feat: added HTTPMethod enum for all api methods

---
+++ PhotoLove/Network/API Structure/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  PhotoLove
+//
+//  Created by Sharad on 27/09/20.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    case PATCH
+}
