3f8facb0be8022a4721dc3e9b2e79305601f582a
Sharad Chauhan <sharad@getinfino.com>
2020-09-28 19:52:02 +0530

    feat: added LoginScreenViewModel and all validations

---
+++ PhotoLove/Modules/Login Screen/View Model/LoginScreenViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  LoginScreenViewModel.swift
+//  PhotoLove
+//
+//  Created by Sharad on 28/09/20.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class LoginScreenViewModel {
+    
+    let validatedEmail: Driver<Bool>
+    let validatedPassword: Driver<Bool>
+    let signInEnabled: Driver<Bool>
+    let isSignInSuccessful: Driver<Bool>
+    
+    init(email: Driver<String>,
+         password: Driver<String>,
+         signInTaps: Signal<()>) {
+        
+        validatedEmail = email
+            .map { email in
+                return email.isValidEmail()
+            }
+        
+        validatedPassword = password
+            .map { password in
+                return password.count > 5
+            }
+        
+        signInEnabled = Driver.combineLatest(validatedEmail, validatedPassword) { isValdEmail, isValidPassword in
+            isValdEmail && isValidPassword
+        }.distinctUntilChanged()
+        
+        let emailAndPassword = Driver.combineLatest(email, password) { (email: $0, password: $1) }
+        isSignInSuccessful = signInTaps.withLatestFrom(emailAndPassword)
+            .flatMapLatest { pair in
+                return FirebaseAuthenticationHandler.performAuthentication(pair.email, pair.password)
+                    .asDriver(onErrorJustReturn: false)
+            }
+    }
+   
+}
