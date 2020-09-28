c25606023a3beefcc41bb1dadec7f3096eea02c6
Sharad Chauhan <sharad@getinfino.com>
2020-09-28 19:48:39 +0530

    feat: added email validation string extension

---
+++ PhotoLove/Extensions/StringExtension.swift
@@ -0,0 +1,17 @@
+//
+//  StringExtension.swift
+//  PhotoLove
+//
+//  Created by Sharad on 28/09/20.
+//
+
+import Foundation
+
+extension String {
+    
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self)
+    }
+}
