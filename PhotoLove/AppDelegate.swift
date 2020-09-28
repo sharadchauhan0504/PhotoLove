43db67cba628c984772176308b88b377bb88081f
Sharad Chauhan <sharad@getinfino.com>
2020-09-28 19:47:19 +0530

    feat: removed scene delegate methods. add code to set root controller. added firebase service initialisation.

--- PhotoLove/AppDelegate.swift
+++ PhotoLove/AppDelegate.swift
@@ -11,25 +11,31 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
-
+    private var services: [UIApplicationDelegate] = [
+        FirebaseService()
+    ]
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        services.forEach { _ = $0.application?(application, didFinishLaunchingWithOptions: launchOptions) }
+        setRootController()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    private func setRootController() {
+        let splashScreenController                                   = SplashScreenController()
+        let navigationController                                     = UINavigationController(rootViewController: splashScreenController)
+        navigationController.isNavigationBarHidden                   = true
+        navigationController.view.backgroundColor                    = .white
+        navigationController.navigationBar.shadowImage               = UIImage()
+        navigationController.navigationBar.tintColor                 = .black
+        navigationController.navigationBar.barTintColor              = .white
+        navigationController.navigationItem.backBarButtonItem?.title = ""
+        window                                                       = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController                                   = navigationController
+        window?.makeKeyAndVisible()
     }
 
     // MARK: - Core Data stack
