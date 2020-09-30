b176e5affb9fcbe160a6f3efbb3e058e18278168
Sharad Chauhan <sharad@getinfino.com>
2020-09-30 13:31:17 +0530

    refactor: added create account firebase auth method

--- PhotoLove/Network/Firebase Auth/FirebaseAuthenticationHandler.swift
+++ PhotoLove/Network/Firebase Auth/FirebaseAuthenticationHandler.swift
@@ -11,13 +11,26 @@ import RxSwift
 
 class FirebaseAuthenticationHandler {
     
-    class func performAuthentication(_ email: String, _ password: String) -> Observable<Bool> {
+    class func createAccount(_ email: String, _ password: String) -> Observable<Bool> {
         return Observable.create { (observer : AnyObserver<Bool>) -> Disposable in
             Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
                 if let error = error {
                     observer.onError(error)
                 } else {
-                    print("User: \(result?.user)")
+                    observer.onNext(true)
+                    observer.onCompleted()
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    class func performAuthentication(_ email: String, _ password: String) -> Observable<Bool> {
+        return Observable.create { (observer : AnyObserver<Bool>) -> Disposable in
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    observer.onError(error)
+                } else {
                     observer.onNext(true)
                     observer.onCompleted()
                 }
